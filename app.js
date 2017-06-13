9bba21dafe2caec6eb8c3b85a9b195ada196a397
Kate Wood <katewood611@gmail.com>
2017-06-12 22:25:27 -0700

    adds comments

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
+// a way to check that our JS file is properly connected to our HTML file
 console.log('connected')
 
+// listens for a click on the main image
 document.getElementById('headshot').addEventListener('click', function() {
+  // sets the image's border color to red
   this.style.borderColor = 'red'
 })
 
+// listens for a click on the "About Me" text
 document.getElementById('title').addEventListener('click', function() {
+  // changes the page's background color to red or hot pink, depending on the current color
   if (document.body.style.backgroundColor == 'red') {
     document.body.style.backgroundColor = 'hotpink'
   } else {
